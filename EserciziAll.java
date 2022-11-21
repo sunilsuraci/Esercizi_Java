291833cb5f9084dd300dd7c273e6bd97018759e8
sunilsuraci <suracisunil@gmail.com>
2022-11-21 17:01:57 +0100

    Pari e dispari Completo

--- EserciziAll.java
+++ EserciziAll.java
@@ -7,19 +7,32 @@ public class EserciziAll {
 
     //Compreso
 
-public static int Compreso (int a, int b, int c){
-    if ((a>b & c<b) || (a<b & c>b)){
-        System.out.println(b + " e' compreso tra " + a +" e " + c);
+    public static int Compreso (int a, int b, int c){
+        if ((a>b & c<b) || (a<b & c>b)){
+            System.out.println(b + " e' compreso tra " + a +" e " + c);
         return b;
-    }else {
-        System.out.println(b + " Non e' compreso tra i due numeri " + a + " e " + c);
+        }else {
+            System.out.println(b + " Non e' compreso tra i due numeri " + a + " e " + c);
         return b;
+        }
+    };
+
+    //Pari e dispari
+
+
+    public static int PariDispari(int a) {
+        if ((a % 2) == 0 ) {
+            System.out.println(a + " e' un numero Pari");
+            
+        } else {
+            System.out.println(a + " Non e' un numero Pari");
+        }return a;
     }
-};
 
 
     public static void main(String[] args){
         Compreso(50,44,14);
+        PariDispari(6);
     }
     
 }
