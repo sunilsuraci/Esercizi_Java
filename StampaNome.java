c71888a8eb3574d8773fe7f2822ded6fe4f1a491
sunilsuraci <suracisunil@gmail.com>
2022-11-21 15:52:46 +0100

    Agg

--- StampaNome.java
+++ StampaNome.java
@@ -2,8 +2,8 @@
 
 public class StampaNome {
     public static void main(String[] args){
-        System.out.println("+ - - - - - +");
         String nome = "Sunil";
+        System.out.println("+ - - - - - +");
         System.out.println("|   "+ nome + "   |");
         System.out.println("+ - - - - - +");
     }
