eb44ea289a6e490e6b5674d568c981d32c0a0a66
sunilsuraci <suracisunil@gmail.com>
2022-11-21 15:50:58 +0100

    ES. nome

---
+++ StampaNome.java
@@ -0,0 +1,10 @@
+
+
+public class StampaNome {
+    public static void main(String[] args){
+        System.out.println("+ - - - - - +");
+        String nome = "Sunil";
+        System.out.println("|   "+ nome + "   |");
+        System.out.println("+ - - - - - +");
+    }
+}
