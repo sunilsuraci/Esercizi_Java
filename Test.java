fb12831e41087a05223765ab7c28df870fd571ce
sunilsuraci <suracisunil@gmail.com>
2022-11-21 12:12:40 +0100

    Prova1

--- Test.java
+++ Test.java
@@ -1,6 +1,9 @@
 public class Test {
     public static void main(String[] args){
-        int Num = 5;
-        for(int i)
+        int num = 5;
+        for(int i=0; i<num; i++){
+            int somma = i + i;
+            System.out.println(somma);
+        }
     }
 }
