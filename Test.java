33c7177f143d82b384a49b5a437672f0680bb3ad
sunilsuraci <suracisunil@gmail.com>
2022-11-21 15:04:02 +0100

    Correzione

--- Test.java
+++ Test.java
@@ -1,10 +1,18 @@
 public class Test {
     public static void main(String[] args){
         int numGiri = 5;
-        for(int i=0; i<numGiri; i++){
+        int num1 = 0;
+        int num2 = 1;
+        int tot = 0;
+        for(int i=0; i<numGiri; ++i){
+            System.out.println(tot);
+            num1 = num2;
+            num2 = tot;
+
+            tot = num1 + num2;
+
             
-            int num2 = i;
-            System.out.println(i+num2);
+            
         }
         
     }
