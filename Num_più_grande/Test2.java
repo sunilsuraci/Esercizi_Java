8ccb250d3a657ff1c39df7e4e37bb27e3d56558b
sunilsuraci <suracisunil@gmail.com>
2022-11-21 15:20:34 +0100

    Es. numero più grande

---
+++ Num_più_grande/Test2.java
@@ -0,0 +1,18 @@
+package Num_più_grande;
+
+
+public class Test2 {
+    public static void main(String[] args){
+        
+    }
+};
+
+public static void ilPiuGrande (int num1, int num2) {
+    if(num1 > num2){
+        System.out.println(num1 + "è più grande");
+    } else {
+        System.out.println(num2 + "è più grande");
+    }
+    return;
+    
+}
