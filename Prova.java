c06a3ffa1bb89c7f6303f731c76193cbff0f22c3
sunilsuraci <suracisunil@gmail.com>
2022-11-22 10:36:26 +0100

    Agg.Funzione

--- Prova.java
+++ Prova.java
@@ -5,15 +5,10 @@ public class Prova {
         Scanner input = new Scanner(System.in);
         System.out.println("Digita un qualsiasi numero: ");
         int a = input.nextInt();
-        System.out.println("digita un altro numero: ");
-        int b = input.nextInt();
-        if (a > b){
-            System.out.println(a + " e' più grande di: " + b);
-        } else if (a < b ){
-            System.out.println(b + " e' più grande di: " + a);
-        } else{
-            System.out.println( a + " sono uguali " + b);
-        }
+        while(a != 0){
+            
+            
+        } 
         
         
         input.close();
