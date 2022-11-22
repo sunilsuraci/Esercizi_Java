82ebf32e7eda20de8c047f7f62db63398639d938
sunilsuraci <suracisunil@gmail.com>
2022-11-22 16:55:03 +0100

    Prova1_matriceDiagonale

--- EserciziMatrice.java
+++ EserciziMatrice.java
@@ -1,25 +1,54 @@
+import java.util.Random;
+
+
+
 public class EserciziMatrice {
     
     public static void main(String[] args){
-       
+        matrice(6,"f");
+    }
+    public static void matrice (int numMatrice, String carattere){ 
+        String  [] [] quadrato = new String[numMatrice][numMatrice];
         
-        int quadrato [] [] = new int[7] [7];
-        int i, j;
-        for(i=0; i < quadrato.length; i++) {
+        /*for(i=0; i < quadrato.length; i++) {
             for(j=0; j < quadrato.length; j=j+1) {
               quadrato[i][j]=0;
             }
+          }*/
+
+
+
+          for(int i=0; i < quadrato.length; i++) {
+            for(int j=0; j < numMatrice; j++) {
+                
+                
+              System.out.print(quadrato[i][j] + " ");
+            }
+            System.out.println(quadrato[i]);
           }
 
-          for(i=0; i < quadrato.length; i++) {
-            for(j=0; j < quadrato.length; j++) {
-              System.out.print(quadrato[i][j]+" ");
-            }
-            //System.out.println(quadrato);
-          }
+        }    
+        
+
+
+          //Matrice random 
+
+          //int random [] [] = new int [4] [3];
+
+
+
+
+
+
+
+
+
+
+          //Matrici
+
 
 
         
-    }
+    
 }
 
